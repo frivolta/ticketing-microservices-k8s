5b631231e74422889747d1fb69f89d0a2ef0683e
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-09-05 11:17:06 +0200

    Adding signin/up/current jwt flow

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,17 @@ import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handlert";
 import { NotFoundError } from "./errors/not-found-error";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,6 +32,9 @@ app.all("*", async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be specified");
+  }
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to Mongo");
