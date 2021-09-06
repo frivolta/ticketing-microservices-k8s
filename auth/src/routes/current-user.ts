6da0bfd109b7d460b99eef7b7d42982b39ed3ea3
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-09-06 17:25:55 +0200

    Refactor to use common library

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,6 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 
-import { currentUser } from "../middlewares/current-user";
-import { requireAuth } from "../middlewares/require-auth";
+import { currentUser } from "@frivolta-tickets/common";
 
 const router = express.Router();
 router.get("/api/users/currentuser", currentUser, (req, res) => {
