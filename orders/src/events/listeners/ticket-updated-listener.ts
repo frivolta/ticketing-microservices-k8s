f2872e90a0a6e3fb68a4ca4946fd01361593dd20
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-09-09 14:26:48 +0200

    Adding listeners to orders

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import {
+  Listener,
+  Subjects,
+  TicketUpdatedEvent,
+} from "@frivolta-tickets/common";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    const { title, price } = data;
+    ticket.set({ title, price });
+    await ticket.save();
+    msg.ack();
+  }
+}
